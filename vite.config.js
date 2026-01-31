68a5b2029903356d31c7285716a728f115ab5562
jgs <wjdrltjr5@naver.com>
2026-01-31 17:26:21 +0900

    setting : tailwind, tanstack query 추가

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   // 서브모듈 env 파일 인식
   envDir: path.resolve(__dirname, './FE-one-stock-config')
 })

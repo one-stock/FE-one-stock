e4fd813e9e9a23ca4a304f3c98d1ea3a126a7047
jgs <wjdrltjr5@naver.com>
2026-02-22 14:15:31 +0900

    feat : 주식  기본 틀 생성 #2

--- src/component/stock/Stock.jsx
+++ src/component/stock/Stock.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
+import StockInfo from './StockInfo';
 
 const Stock = () => {
     return (
-        <div className='bg-main-color w-full h-full p-2'>
-            <div className='flex-col w-full bg-sub-color rounded-lg'>
-                <div className='flex w-full justify-end'>
-                    <div className='w-full'>
-                    삼성전자
-                    </div>
-                    <div className='w-full'>
-                    하트
-                    </div>
-                </div>
-                <div>
-                    금액
-                </div>
-            </div>
+        <div className='w-full h-full p-2 overflow-auto'>
+            <StockInfo/>
         </div>
     );
 };
--- src/page/common/Layout.jsx
+++ src/page/common/Layout.jsx
@@ -6,14 +6,14 @@ import Bottom from './Bottom';
 const Layout = () => {
     return (
         <div className='min-h-dvh w-screen flex justify-center'>
-            <div className="w-full bg-main flex flex-col min-h-dvh max-w-120">
-                <header className="fixed max-w-120 top-0 left-1/2 -translate-x-1/2 w-full  h-16 border-b flex items-center bg-main-color z-50">
+            <div className="w-full bg-main-color flex flex-col min-h-dvh max-w-120">
+                <header className="fixed max-w-120 top-0 left-1/2 -translate-x-1/2 w-full  h-16 flex items-center bg-main-color z-50">
                     <Header/>
                 </header>
-                <main className="flex grow pt-16 pb-20 h-full w-full bg-main">
+                <main className="flex grow pt-16 pb-16 h-full w-full bg-main">
                     <Outlet/>                
                 </main>
-                <footer className="fixed max-w-120 bottom-0 w-full left-1/2 -translate-x-1/2 h-20 border-t flex justify-around items-center bg-bottom-color z-50">
+                <footer className="fixed max-w-120 bottom-0 w-full left-1/2 -translate-x-1/2 h-16 flex justify-around items-center bg-bottom-color z-50">
                     <Bottom/>
                 </footer>   
             </div>

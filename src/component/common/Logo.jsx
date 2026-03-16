03aeb90e96d2c0984def76cff96c88e77328b2b0
jgs <wjdrltjr5@naver.com>
2026-03-16 16:52:31 +0900

    feat : header logo 적용

---
+++ src/component/common/Logo.jsx
@@ -0,0 +1,12 @@
+import LogoPng from '../../../public/logo.png';
+
+const Logo = () => {
+  return (
+    <img className='w-25 mt-0.5'
+      src={LogoPng} 
+      alt={"logo"} 
+    />
+  );
+};
+
+export default Logo;
--- src/page/common/Header.jsx
+++ src/page/common/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from '../../component/common/Logo';
 
 const Header = () => {
     return (
-        <div className='p-2 w-full flex justify-between'>
+        <div className='p-2 w-full flex justify-between items-center'>
             <div>
-                Logo
+                <Logo/>
             </div>
             <div>
                 햄버거메뉴

d2c24525d932e69d9de3d8819532e1ec9bcb3b39
jgs <wjdrltjr5@naver.com>
2026-03-17 21:28:07 +0900

    feat : 뉴스 공시 탭

--- src/component/stock/Information/StockMainInfo.jsx
+++ src/component/stock/Information/StockMainInfo.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const StockMainInfo = () => {
     return (
-        <div className='mt-10'>
-            Main 자리
-        </div>
+            <div className='flex flex-col mt-10'>
+                    <div className='flex justify-around bg-bottom-color rounded-t-xl p-1 gap-2'>
+                        <div>
+                            <button type='button'>뉴스</button>
+                        </div>
+                        <div>
+                            <button type='button'>공시 </button>
+                        </div>
+                    </div>
+            </div>
     );
 };
 

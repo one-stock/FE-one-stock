49237c5d80b5096d7204fe23a8eb7c9f73629aab
jgs <wjdrltjr5@naver.com>
2026-03-25 20:15:15 +0900

    feat : stock info prototype

--- src/component/stock/Information/Disclosure/StocksDisclosureItem.jsx
+++ src/component/stock/Information/Disclosure/StocksDisclosureItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const StocksDisclosureItem = () => {
     return (
-        <div className='bg-main-color w-full rounded-xl p-2 text-center flex justify-around'>
+        <Link className='bg-main-color w-full rounded-xl p-2 text-center flex justify-around' to='https://dart.fss.or.kr/dsaf001/main.do?rcpNo=20200117000375'>
             <div>임원ㆍ주요주주특정증권등소유상황보고서</div>
             <div>2020-01-17</div>
-        </div>
+        </Link>
     );
 };
 

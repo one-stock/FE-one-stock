6eaaf98e157bb04a2b11b727e6e1675ba28fa3e4
jgs <wjdrltjr5@naver.com>
2026-03-26 19:34:45 +0900

    feat : 리뷰 지적사항 적용

--- src/component/stock/Information/Disclosure/StocksDisclosure.jsx
+++ src/component/stock/Information/Disclosure/StocksDisclosure.jsx
@@ -8,13 +8,15 @@ const StocksDisclosure = () => {
         };
     return (
         <div className='flex flex-col gap-2 items-center w-full'>
-                    <div className='flex flex-col gap-2 w-full'>
-                        {Array.from({ length: itemCount }).map((_, index) => (
-                            <StocksDisclosureItem key={index} />
-                        ))}
-                    </div>
-                    <div className='bg-main-color rounded-xl p-2 min-w-20 flex justify-center'><button className='w-full' onClick={handleMoreClick}>더보기</button></div>
-                </div>
+            <div className='flex flex-col gap-2 w-full'>
+                {Array.from({ length: itemCount }).map((_, index) => (
+                    <StocksDisclosureItem key={index} />
+                ))}
+            </div>
+            <div className='bg-main-color rounded-xl p-2 min-w-20 flex justify-center'>
+                <button className='w-full' onClick={handleMoreClick}>더보기</button>
+            </div>
+        </div>
     );
 };
 
--- src/component/stock/Information/StockMainInfo.jsx
+++ src/component/stock/Information/StockMainInfo.jsx
@@ -5,20 +5,14 @@ import StocksDisclosure from './Disclosure/StocksDisclosure';
 const StockMainInfo = () => {
     const [flag, setFlag] = useState("NEWS");
 
-    const handleFlag = (flag) => {
-        setFlag(flag)
-    }
-
-
-
     return (
             <div className='flex flex-col mt-10'>
                     <div className='flex justify-around bg-bottom-color rounded-t-xl p-1 gap-2'>
                         <div>
-                            <button type='button' onClick={() => handleFlag("NEWS")}>뉴스</button>
+                            <button type='button' onClick={() => setFlag("NEWS")}>뉴스</button>
                         </div>
                         <div>
-                            <button type='button' onClick={() => handleFlag("DISCLOSURE")}>공시 </button>
+                            <button type='button' onClick={() => setFlag("DISCLOSURE")}>공시 </button>
                         </div>
                     </div>
                     <div className='bg-sub-color p-2 rounded-b-xl'>
